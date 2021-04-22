95f1706275d5c94fd67b1052b827d6476b3f55ca
brillianz <brizamora68@gmail.com>
2021-04-22 13:53:08 -0700

    lab3.sub

--- Week_03/initlabhw.js
+++ Week_03/initlabhw.js
@@ -11,3 +11,20 @@ addMarker(34.29485621668343, -118.44404696035478, 'Rudy Ortega Park','Northeast 
 addMarker(34.26878011971803, -118.43807575242879, 'Paxton Park','Northeast San Fernando Valley Parks')
 addMarker(34.28123973431585, -118.43340039258449, 'San Fernando Recreation Park','Northeast San Fernando Valley Parks')
 addMarker(34.31744378085456, -118.4148202830943, 'El Cariso Park','Northeast San Fernando Valley Parks')
+
+
+
+// adding markers
+let work = L.marker([34.0709, -118.444]).addTo(map)
+		.bindPopup('Where I work on campus')
+
+let home = L.marker([37.7409, -122.484]).addTo(map)
+		.bindPopup('Where I currently am')
+
+let random = L.marker([39.7409, -122.484]).addTo(map)
+		.bindPopup('Third Point')
+function addMarker(lat,lng,message){
+        console.log(message)
+        L.marker([lat,lng]).addTo(map).bindPopup(message)
+        return message
+}   
--- Week_03/js/init.js
+++ Week_03/js/init.js
@@ -21,10 +21,29 @@ function addMarker(lat,lng,message){
         L.marker([lat,lng]).addTo(map).bindPopup(message)
         return message
 }   
-addMarker(37,-122,'you are awesome! you automated a marker function') 
 
 
-// let gym = L.marker(34.28119255680654, -118.43808847729682).addTo(map)
-// let school = L.marker(34.07468359809358, -118.43964468373282).addTo(map)
-// let food = L.marker(34.170801126874615, -118.33886777787623).addTo(map)
-addMarker(34.28119255680654, -118.43808847729682, 'gym')
+
+addMarker(34.28740046564392, -118.4507422217334, 'Las Palmas park','Northeast San Fernando Valley Parks')
+addMarker(34.29485621668343, -118.44404696035478, 'Rudy Ortega Park','Northeast San Fernando Valley Parks')
+addMarker(34.26878011971803, -118.43807575242879, 'Paxton Park','Northeast San Fernando Valley Parks')
+addMarker(34.28123973431585, -118.43340039258449, 'San Fernando Recreation Park','Northeast San Fernando Valley Parks')
+addMarker(34.31744378085456, -118.4148202830943, 'El Cariso Park','Northeast San Fernando Valley Parks')
+
+function addMarker(lat, lng, title, message){
+    console.log(message)
+    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2>`)
+    createButtons(lat,lng,title); // new line!!!
+    return message
+} 
+function createButtons(lat,lng,title){
+    const newButton = document.createElement("button"); // adds a new button
+    newButton.id = "button"+title; // gives the button a unique id
+    newButton.innerHTML = title; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
+    newButton.addEventListener('click', function(){
+        map.flyTo([lat,lng]); //this is the flyTo from Leaflet
+    })
+    document.body.appendChild(newButton); //this adds the button to our page.
+}
