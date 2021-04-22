ebd5307766858167832358699168600b30bca26b
brillianz <brizamora68@gmail.com>
2021-04-22 13:55:05 -0700

    lab3

--- Week_03/js/init.js
+++ Week_03/js/init.js
@@ -8,14 +8,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // adding markers
-let work = L.marker([34.0709, -118.444]).addTo(map)
-		.bindPopup('Where I work on campus')
-
-let home = L.marker([37.7409, -122.484]).addTo(map)
-		.bindPopup('Where I currently am')
-
-let random = L.marker([39.7409, -122.484]).addTo(map)
-		.bindPopup('Third Point')
 function addMarker(lat,lng,message){
         console.log(message)
         L.marker([lat,lng]).addTo(map).bindPopup(message)
